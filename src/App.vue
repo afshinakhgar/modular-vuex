0bdfb9945478638cf495c1d2cfdf9ed335a070e1
Afshin Akhgar <afshin.akhgar@gmail.com>
2016-09-14 18:12:20 +0430

    add login action Do login action

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+
+
+    <button @click="doLogin">Login</button>
   </div>
 </template>
 
 <script>
 import store from './store'
+import {doLogin} from './modules/login/actions'
 export default {
   data () {
     return {
       msg: 'Hello Vue!'
     }
   },
+  vuex : {
+  	actions : {
+  		doLogin
+  	}
+  }
   store
 }
 </script>
